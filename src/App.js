8bdeb9355f953442a546b6cdddcb946dc557e5a4
rohitcode005 <rohitcode005@gmail.com>
2021-09-26 02:02:03 +0530

    fetch data from firebase

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
 import './App.css';
+import "firebase/compat/firestore";
+import firebase from "./Firebase";
+import React, { useState, useEffect } from "react";
+
 
 function App() {
+
+  const [user, setUser] = useState([]);
+  const [loading, setloading] = useState(false);
+
+  const ref = firebase.firestore().collection("users");
+
+  function getUsers() {
+    setloading(true);
+    ref.onSnapshot((qs) => {
+      const item = [];
+      qs.forEach((doc) => {
+        item.push(doc.data());
+      });
+      setloading(false);
+      setUser(item);
+    });
+  }
+
+  if (loading) {
+    return <h>Loading...</h>;
+  }
+
+
+
+  useEffect(() => {
+    getUsers();
+  }, []);
+
+
+  // docc.get().then((qs) => {
+  //   qs.forEach(i => {
+  //     console.log(i.data().name);
+  //     return (<p>{i.data().name}</p>);
+  //   })
+  // });
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Hello Firebase</h1>
+      {user.map((element) => {
+        <div key={element.id}>
+          <h2>{user.name}</h2>
+          <h3>{user.class}</h3>
+        </div>
+      })}
     </div>
   );
 }
---
+++ src/Firebase.js
@@ -0,0 +1,20 @@
+import firebase from "firebase/compat/app";
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyANasgb6UtosJMKknlOvbHQoEcS6qyYIcM",
+    authDomain: "reactapp-af918.firebaseapp.com",
+    projectId: "reactapp-af918",
+    storageBucket: "reactapp-af918.appspot.com",
+    messagingSenderId: "969605914417",
+    appId: "1:969605914417:web:29bb442c02bdf3d9079a10",
+    measurementId: "G-5X1LWRXH1E"
+};
+
+// Initialize Firebase
+
+
+firebase.initializeApp(firebaseConfig);
+
+
+export default firebase;
+
